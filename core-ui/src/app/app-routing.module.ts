269f041135006d9b333fbed0e428ddfb19e2bf8a
DenisErmolovich <denisermolovich@fico.com>
2019-09-13 15:50:25 +0300

    our-game/core-ui: auth
    Add GameRouting module
    
    [#12]

--- core-ui/src/app/app-routing.module.ts
+++ core-ui/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home/home.component';
-import {GameListComponent} from './game/layout/game-list/game-list.component';
-import {GameComponent} from './game/layout/layout-with-score/game/game.component';
-import {RoundComponent} from './game/layout/layout-with-score/round/round.component';
-import {SuperRoundComponent} from './game/layout/layout-with-score/super-round/super-round.component';
-import {QuestionComponent} from './game/layout/layout-with-score/question/question.component';
 import {LoginComponent} from './auth/login/login.component';
 
 const routes: Routes = [
@@ -13,26 +8,6 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
-  },
-  {
-    path: 'game',
-    component: GameListComponent
-  },
-  {
-    path: 'game/:gameId',
-    component: GameComponent
-  },
-  {
-    path: 'game/:gameId/round/super',
-    component: SuperRoundComponent
-  },
-  {
-    path: 'game/:gameId/round/:roundId',
-    component: RoundComponent
-  },
-  {
-    path: 'game/:gameId/round/:roundId/question/:questionId',
-    component: QuestionComponent
   },
   {
     path: 'login',
--- core-ui/src/app/game/game.module.ts
+++ core-ui/src/app/game/game.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GameComponent } from './layout/layout-with-score/game/game.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GameComponent} from './layout/layout-with-score/game/game.component';
 import {GameListComponent} from './layout/game-list/game-list.component';
-import { LayoutComponent } from './layout/layout.component';
+import {LayoutComponent} from './layout/layout.component';
 import {RouterModule} from '@angular/router';
-import { LayoutWithScoreComponent } from './layout/layout-with-score/layout-with-score.component';
-import { RoundComponent } from './layout/layout-with-score/round/round.component';
-import { SuperRoundComponent } from './layout/layout-with-score/super-round/super-round.component';
-import { QuestionComponent } from './layout/layout-with-score/question/question.component';
+import {LayoutWithScoreComponent} from './layout/layout-with-score/layout-with-score.component';
+import {RoundComponent} from './layout/layout-with-score/round/round.component';
+import {SuperRoundComponent} from './layout/layout-with-score/super-round/super-round.component';
+import {QuestionComponent} from './layout/layout-with-score/question/question.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {GameRoutingModule} from './game-routing.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,9 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [
     CommonModule,
     RouterModule,
+    GameRoutingModule,
     ReactiveFormsModule
   ]
 })
-export class GameModule { }
+export class GameModule {
+}
