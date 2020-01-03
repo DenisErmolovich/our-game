94b7690c36107d5b04d5f7637d2188c550cbc6d8
DenisErmolovich <denisermolovich@gmail.com>
2020-01-03 17:37:04 +0300

    our-game/core-ui: Login as a default page.
    app-routing.module.ts: delete "/login" path, make LoginComponent default page.
    
    [#18]

--- core-ui/src/app/app-routing.module.ts
+++ core-ui/src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home/home.component';
 import {LoginComponent} from './auth/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: LoginComponent,
     pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: LoginComponent
   }
 ];
 
