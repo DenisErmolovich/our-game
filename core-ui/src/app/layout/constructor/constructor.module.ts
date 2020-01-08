b9609b6c38ae409be3503adf96216dfa09d4d1b7
DenisErmolovich <denisermolovich@gmail.com>
2020-01-08 14:01:04 +0300

    our-game/core-ui: add game-list component.
    [#18]

--- core-ui/src/app/layout/constructor/constructor.module.ts
+++ core-ui/src/app/layout/constructor/constructor.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameListComponent } from './game-list/game-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GameListComponent],
   imports: [
     CommonModule
   ]
