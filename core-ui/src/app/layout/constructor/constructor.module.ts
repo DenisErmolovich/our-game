f3589f1ee57f22a4c8ec9863bf244d117e5c1367
DenisErmolovich <denisermolovich@gmail.com>
2020-01-10 16:08:49 +0300

    our-game/game-server: add game editor component

--- core-ui/src/app/layout/constructor/constructor.module.ts
+++ core-ui/src/app/layout/constructor/constructor.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameListComponent } from './game-list/game-list.component';
 import {ConstructorRoutingModule} from './constructor-routing.module';
+import { GameEditorComponent } from './game-editor/game-editor.component';
+import {SharedModule} from '../../_shared/shared.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [GameListComponent],
+  declarations: [
+    GameListComponent,
+    GameEditorComponent
+  ],
   imports: [
     CommonModule,
-    ConstructorRoutingModule
+    ConstructorRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class ConstructorModule { }
--- core-ui/src/app/layout/constructor/game-editor/game-editor.component.ts
+++ core-ui/src/app/layout/constructor/game-editor/game-editor.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
+import {CustomValidators} from '../../../_validation/custom-validators';
+import {ValidationUtil} from '../../../_validation/validation-util';
+import {Location} from '@angular/common';
+import {Game} from "../../../_models/game";
+import {GameService} from "../../../_services/data/http/game.service";
+import {BalloonErrorService} from "../../../_services/errors/balloon-error.service";
 
 @Component({
   selector: 'app-game-redactor',
@@ -6,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-editor.component.scss']
 })
 export class GameEditorComponent implements OnInit {
+  private _form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private location: Location,
+    private gameService: GameService,
+    private errorService: BalloonErrorService
+  ) { }
 
   ngOnInit() {
+    this._form = this.initForm();
   }
 
+  get form(): FormGroup {
+    return this._form;
+  }
+
+  onSubmit(): void {
+    this._form.markAllAsTouched();
+    if (this._form.invalid) {
+      return;
+    }
+    const name = this._form.controls.name.value;
+    this.saveGame({name});
+  }
+
+  getValidationMessage(controlName: string): string {
+    const control: AbstractControl = this._form.controls[controlName];
+    return ValidationUtil.getCustomErrorsFromControl(control);
+  }
+
+  private initForm(): FormGroup {
+    return this.fb.group({
+      name: ['', [CustomValidators.required()]]
+    });
+  }
+
+  private saveGame(game: Game): void {
+    this.gameService.save(game).subscribe(
+      response => this.location.back(),
+      error => this.errorService.sendError(JSON.stringify(error))
+    );
+  }
 }
