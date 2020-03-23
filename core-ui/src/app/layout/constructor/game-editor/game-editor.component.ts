32270e915722273198a331961e709b59923243df
DenisErmolovich <denisermolovich@gmail.com>
2020-03-23 12:02:22 +0300

    our-game/core-ui: WIP
    [#18]

--- core-ui/src/app/layout/constructor/game-editor/game-editor.component.ts
+++ core-ui/src/app/layout/constructor/game-editor/game-editor.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup} from '@angular/forms';
 import {CustomValidators} from '../../../_validation/custom-validators';
 import {ValidationUtil} from '../../../_validation/validation-util';
 import {Location} from '@angular/common';
-import {Game} from "../../../_models/game";
-import {GameService} from "../../../_services/data/http/game.service";
-import {BalloonErrorService} from "../../../_services/errors/balloon-error.service";
+import {Game} from '../../../_models/game';
+import {GameService} from '../../../_services/data/http/game.service';
+import {BalloonErrorService} from '../../../_services/errors/balloon-error.service';
+import {ActivatedRoute} from '@angular/router';
+import {flatMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-redactor',
@@ -14,20 +16,32 @@ import {BalloonErrorService} from "../../../_services/errors/balloon-error.servi
 })
 export class GameEditorComponent implements OnInit {
   private _form: FormGroup;
+  private _id: string;
 
   constructor(
     private fb: FormBuilder,
     private location: Location,
     private gameService: GameService,
-    private errorService: BalloonErrorService
+    private errorService: BalloonErrorService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this._form = this.initForm();
+    this.route.queryParamMap.pipe(
+      flatMap(params => params.get('id'))
+    ).subscribe(id => {
+      this._id = id;
+      this._form = this.initForm();
+      this.fillForm(this._form, {id: '1', name: 'boo'});
+    });
   }
 
   get form(): FormGroup {
     return this._form;
+  }
+
+  get id(): string {
+    return this._id;
   }
 
   onSubmit(): void {
@@ -47,6 +61,15 @@ export class GameEditorComponent implements OnInit {
   private initForm(): FormGroup {
     return this.fb.group({
       name: ['', [CustomValidators.required()]]
+    });
+  }
+
+  private fillForm(form: FormGroup, object: object): void {
+    Object.keys(object).forEach(key => {
+      const control = form.controls[key];
+      if (control) {
+        control.setValue(object[key]);
+      }
     });
   }
 
